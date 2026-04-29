cdfd51687ff37d73c9632dd6473fce847d414ac4
Andreas Engstrand <andreas.engstrand@protonmail.com>
2026-04-29 18:56:22 +0200

    RCC-C3DOCS: remap to canonical INU SVG suffixes (PV/SP/CSP/OP/HAL/AHL/DB/SPC/MNL/MXL/P/I/CMD/MCMD/M/OPM/TD)

--- wpp.meta.js
+++ wpp.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         INU WebPort-Plus
 // @namespace    http://tampermonkey.net/
-// @version      7.3.20260429.1821
+// @version      7.3.20260429.1856
 // @description  Enhanced UI for Kiona WebPort
 // @match        *://*/*
 // @grant        GM_setValue
