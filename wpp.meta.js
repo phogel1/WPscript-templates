62761c101f1bcb6c2d06225bd7ed091b59c12475
Andreas Engstrand <andreas.engstrand@protonmail.com>
2026-05-03 12:18:47 +0200

    Regio Midi: comm_fail_al uses {!Q5} (corrected from misread {IQ5})

--- wpp.meta.js
+++ wpp.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         INU WebPort-Plus
 // @namespace    http://tampermonkey.net/
-// @version      7.4.20260503.1216
+// @version      7.4.20260503.1218
 // @description  Enhanced UI for Kiona WebPort
 // @match        *://*/*
 // @grant        GM_setValue
