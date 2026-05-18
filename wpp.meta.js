0061855c32c1dac8f44aed391c5fe51ea2b86ed9
Andreas Engstrand <andreas.engstrand@protonmail.com>
2026-05-18 20:42:18 +0200

    v7.4.20260518.2042

--- wpp.meta.js
+++ wpp.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         INU WebPort-Plus
 // @namespace    http://tampermonkey.net/
-// @version      7.4.20260506.0837
+// @version      7.4.20260518.2042
 // @description  Enhanced UI for Kiona WebPort
 // @match        *://*/*
 // @grant        GM_setValue
